b8f05a43ec528e9ab97aeb91741ca2fd2a896efb
Felipe <felipemeirelessanta@gmail.com>
2021-12-31 17:15:33 -0300

    c1 - Commitando o final do curso

--- Aula16ex/script.js
+++ Aula16ex/script.js
@@ -13,6 +13,46 @@ function isNumero(n) {
 
 function inLista(n, l) {
     if(l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
 
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+       valores.push(Number(num.value))
+       let item = document.createElement('option')
+       item.text = `Valor ${num.value} adicionado.`
+       lista.appendChild(item)
+       res.innerHTML = ''
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+               maior = valores[pos]
+            if(valores[pos] < menor)
+            menor = valores[pos]
+        }
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
     }
 }
