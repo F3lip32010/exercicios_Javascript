e9423ac9c2f028b0f80c403196405ad8c1f49c32
Felipe <felipemeirelessanta@gmail.com>
2022-01-12 16:53:57 -0300

    c4 - Commitando meus exercicios da udemy

---
+++ exercicios - JS/fundamentos/comentario.js
@@ -0,0 +1,15 @@
+// Comentário de uma linha
+console.log('Linha 1')
+
+/*
+ Comentário de
+ múltiplas linhas 
+*/
+console.log('Linha 2')
+
+/*
+ * Comentaŕio de
+ * múltiplas linhas
+ */
+console.log('Linha 3')
+
---
+++ exercicios - JS/fundamentos/organizacao.js
@@ -0,0 +1,8 @@
+console.log("Sentença de código")
+
+{
+    {
+        console.log("OLá")
+        console.log('Mundo!') // Padrão do curso
+    }
+}
