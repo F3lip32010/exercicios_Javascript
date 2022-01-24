e5c6898da957bc56afce90cfef38b99a4db3835c
Felipe <felipemeirelessanta@gmail.com>
2022-01-24 17:26:37 -0300

    c5 - commiting the javascript exercises

---
+++ exercicios - JS/fundamentos/destructuring1.js
@@ -0,0 +1,25 @@
+// novo recurso do ES2015
+
+const pessoa = {
+    nome: 'Ana',
+    idade: 5,
+    endereco: {
+        logradouro: 'Rua ABC',
+        numero: 10000
+    }
+}
+
+const { nome, idade } = pessoa
+console.log(nome, idade)
+
+const { nome: n, idade: i} = pessoa
+console.log(n, i)
+
+const { sobrenome, bemHumorada = true} = pessoa
+console.log(sobrenome, bemHumorada)
+
+const { endereco: { logradouro, numero, cep } } = pessoa
+console.log(logradouro, numero, cep)
+
+const { conta: {AbortSignal, num } } = pessoa
+console.log(ag, num)
---
+++ exercicios - JS/fundamentos/relacionais.js
@@ -0,0 +1,3 @@
+console.log('01)', '1' == 1)
+console.log('02)', '1' === 1)
+console.log('03)', '3' != 3)
